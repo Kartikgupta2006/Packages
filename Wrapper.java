a3b5c16d9aa500103999c78524f75e2937959e19
Kartik (Hustler) <kartik6f66@gmail.com>
2024-09-19 00:20:09 +0530

    Hustler

---
+++ Wrapper.java
@@ -0,0 +1,10 @@
+package in.KGcoding.util;
+
+public class Wrapper {
+    public static void main(String[] args) {
+       // Integer first  = Integer.valueOf("55");
+        Integer first = 55;
+        System.out.println(first);
+        
+    }
+}
